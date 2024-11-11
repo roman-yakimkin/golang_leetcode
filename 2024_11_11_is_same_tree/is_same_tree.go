7264fa600f0d310775478cd886caf90b8549e69d
Roman Yakimkin <r.yakimkin@yandex.ru>
2024-11-11 10:22:01 +0300

    100: Add IsSame tree task

---
+++ 2024_11_11_is_same_tree/is_same_tree.go
@@ -0,0 +1,26 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func IsSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if (p == nil && q != nil) || (p != nil && q == nil) {
+		return false
+	}
+	return p.Val == q.Val && IsSameTree(p.Left, q.Left) && IsSameTree(p.Right, q.Right)
+}
